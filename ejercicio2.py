ef3b0763dd74b24682da0d07631abae5d9036d63
Ivo Juarez <75588359+Vanzo93@users.noreply.github.com>
2024-02-22 20:43:21 -0300

    Segundo Commit POO_clase2

--- ejercicio2.py
+++ ejercicio2.py
@@ -9,25 +9,32 @@ class Animal:
         self.cantidad_patas=cantidad_patas
         self.tipo=tipo
         
-
     def comer(self):
         print("Estoy comiendo")
         
 
+
 class Perro(Animal):
     def __init__(
             self,
+            cantidad_patas: int,
+            tipo: str,
             nombre: str,
             raza: str
             ):
         self.nombre=nombre
         self.raza=raza
-
+        super().__init__(cantidad_patas, tipo)
     def correr(self):
         print("Estoy corriendo")
 
+
 class Aguila(Animal):
-    def __init__(self) -> None:
-        pass
+    def __init__(
+            cantidad_patas: int,
+            tipo: str,
+            self
+            ):
+        super().__init__(cantidad_patas, tipo)
     def volar(self):
         print("Estoy volando")
